fd2b743cf8fd1027ebc87b6177d22327e9a9d2e8
SzymonAuguscik <szyauguscik@gmail.com>
2023-01-27 20:43:53 +0100

    Fix data loading time measurement for DbscanCosFamily

--- Algorithms/source/DbscanCosFamily.cpp
+++ Algorithms/source/DbscanCosFamily.cpp
@@ -8,13 +8,9 @@ namespace Algorithms
 
 void DbscanCosFamily::loadData()
 {
-    const auto startTime = std::chrono::high_resolution_clock::now();
     std::for_each(_points.begin(),
                   _points.end(),
                   [&] (const Data::Point& p_point) { _lengths.insert({p_point.getId(), Data::getLength(p_point)}); });
-    const auto endTime = std::chrono::high_resolution_clock::now();
-    const auto elapsedTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
-    _stats.timeStats.dataLoadingTime = elapsedTime.count();
 }
 
 }
